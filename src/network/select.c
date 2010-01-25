e3cf41cb9766bd6897e2a90b5b2b0a144b5ba0ae
Jan Vidar Krey <janvidar@extatic.org>
2010-01-25 14:23:22 +0100

    BSD/OSX compile fix.

--- src/network/select.c
+++ src/network/select.c
@@ -101,8 +101,8 @@ int net_backend_process()
 	FD_ZERO(&g_backend->wfds);
 
 	size_t secs = timeout_queue_get_next_timeout(&g_backend->timeout_queue, g_backend->now);
-	tval.tv_secs = secs;
-	tval.tv_usecs = 0;
+	tval.tv_sec = secs;
+	tval.tv_usec = 0;
 
 	for (n = 0, found = 0; found < g_backend->num && n < g_backend->max; n++)
 	{
