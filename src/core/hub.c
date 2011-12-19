ba26f4c5e22f3e7947cc37950f4c33a8060094d0
Jan Vidar Krey <janvidar@extatic.org>
2011-12-19 10:54:47 +0100

    Update copyright notices and added licenses to files that were missing it.

--- src/core/hub.c
+++ src/core/hub.c
@@ -1,6 +1,6 @@
 /*
  * uhub - A tiny ADC p2p connection hub
- * Copyright (C) 2007-2010, Jan Vidar Krey
+ * Copyright (C) 2007-2011, Jan Vidar Krey
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/plugins/mod_logging.c
+++ src/plugins/mod_logging.c
@@ -1,7 +1,22 @@
-/**
- * A logging plugin for uhub.
- * Logs to either file or syslog.
+/*
+ * uhub - A tiny ADC p2p connection hub
+ * Copyright (C) 2007-2011, Jan Vidar Krey
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+
 #include "system.h"
 #include "adc/adcconst.h"
 #include "adc/sid.h"
