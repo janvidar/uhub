7706e1cb8a59b74debae0a32481bff4187f2bfbe
Jan Vidar Krey <janvidar@extatic.org>
2014-05-14 11:38:08 +0200

    Fix copyright year.

--- src/tools/adcclient.h
+++ src/tools/adcclient.h
@@ -1,6 +1,6 @@
 /*
  * uhub - A tiny ADC p2p connection hub
- * Copyright (C) 2007-2013, Jan Vidar Krey
+ * Copyright (C) 2007-2014, Jan Vidar Krey
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
