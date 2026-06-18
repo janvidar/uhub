16ee41170276f580b4104b8a74f36c160b1d9947
Jan Vidar Krey <janvidar@extatic.org>
2026-06-18 14:25:34 +0200

    cleanup: uhub-passwd read argv[2] before argc.

--- src/tools/uhub-passwd.c
+++ src/tools/uhub-passwd.c
@@ -404,14 +404,15 @@ int main(int argc, char** argv)
 {
 	size_t n = 0;
 	binary = argv[0];
-	filename = argv[1];
-	command = argv[2];
 
 	if (argc < 3)
 	{
 		main_usage(argv[0]);
 		return 1;
 	}
+
+	filename = argv[1];
+	command = argv[2];
 
 	for (; n < sizeof(COMMANDS) / sizeof(COMMANDS[0]); n++)
 	{
